68168335e66e2531dc00b818d5923ebdcd07b377
tarielDev <tariel@post.com>
2022-05-01 23:14:53 +0400

    Организация ввода и вывода в консоли на кириллице

--- Lesson-1/Program.cs
+++ Lesson-1/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Lesson_1
+﻿using System;
+using System.Text;
+
+namespace Lesson_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Для вывода в консоли на кириллице
+            Console.OutputEncoding = Encoding.UTF8;
+            // Для ввода в консоли на кириллице
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+
+
+
+
         }
     }
 }
